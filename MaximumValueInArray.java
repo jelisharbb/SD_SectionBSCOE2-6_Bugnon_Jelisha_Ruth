ebed990d0561537c19bb90f984f431ef9a2a0540
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-06-07 15:59:44 +0800

    Initial commit

---
+++ MaximumValueInArray.java
@@ -0,0 +1,15 @@
+// Finding the Maximum Value in an Array:
+
+public class MaximumValueInArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 5, 7, 9, 2, 4, 6, 8};
+        int max = numbers[0];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max)
+            max = numbers[i];
+        }
+            
+        System.out.println("The maximum value in the array is " + max + ".");
+    }
+}
